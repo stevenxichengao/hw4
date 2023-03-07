3c5a5edde4ae5de55b9eaa81b4461d9dfb4941a4
Xichen Gao <xichenga@usc.edu>
2023-03-07 00:38:11 +0000

    finished

--- equal-paths.cpp
+++ equal-paths.cpp
@@ -8,11 +8,22 @@ using namespace std;
 
 
 // You may add any prototypes of helper functions here
-
+int process(Node * root){
+	if(root==NULL) return 0;
+	int rheight = process(root->right);
+	int lheight = process(root->left);
+	if(lheight==-1) return -1;
+	if(rheight==-1) return -1;
+	if(lheight==rheight) return lheight+1;
+	if(lheight==0) return rheight+1;
+	if(rheight==0) return lheight+1;
+	return -1;
+}
 
 bool equalPaths(Node * root)
 {
     // Add your code below
-
+    if(process(root)==-1) return false;
+		return true;
 }
 
